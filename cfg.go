15056d0fcfcc6655f5777a1b0efec3b5cae31494
Michael Wolber <mwolber@gmx.de>
2025-08-13 17:11:14 +0200

    Fixing version/timestamp trick

--- cfg.go
+++ cfg.go
@@ -12,7 +12,7 @@ import (
 var (
 	AppName        = "<unknown app name>"
 	Version        = "<unknown build version>"
-	BuildTimeStamp = "<unknown build timestamp>"
+	BuildTimestamp = "<unknown build timestamp>"
 	versionFlag    *bool
 )
 
@@ -27,17 +27,15 @@ func Configure() {
 
 func ShowVersion() {
 
-	// Get the name of the application from the file path
+	// Get the name of the application from the file path in the call stack
 	_, file, _, _ := runtime.Caller(2)
 	AppName = strings.Split(file, "/")[len(strings.Split(file, "/"))-2]
 
-	if Version == "" {
-		Version = "<unknown git version>"
-	}
-	btime, err := time.Parse("2006-01-02_15:04:05_MST", BuildTimeStamp)
+	btime, err := time.Parse("2006-01-02_15:04:05_MST", BuildTimestamp)
 	if err != nil {
-		btime = time.Now()
+		fmt.Printf("%s - version %s built on %s \n", AppName, Version, BuildTimestamp)
+	} else {
+		fmt.Printf("%s - version %s built on %s \n", AppName, Version, btime.Format("02.01.2006 - 15:04:05 MST"))
 	}
-	fmt.Printf("%s - version %s built on %s \n", AppName, Version, btime.Format("02.01.2006 - 15:04:05 MST"))
 	os.Exit(0)
 }
